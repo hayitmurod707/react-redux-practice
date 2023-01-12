395a99402baf7640804d2e175424175e0c3fbf90
hayitmurod707 <hayitmurod707@gmail.com>
2023-01-12 10:41:37 +0500

    updated

--- src/Redux/index.js
+++ src/Redux/index.js
@@ -2,18 +2,19 @@ import { configureStore } from '@reduxjs/toolkit';
 import logger from "redux-logger";
 import counter from './counter';
 import users from './users';
+const env = process.env.NODE_ENV === 'development'
 const store = configureStore({
   reducer: {
     counter,
 		users,
   },
 	middleware: getDefaultMiddleware => {
-		if(process.env.NODE_ENV === 'development') {
+		if(env) {
 			return getDefaultMiddleware({ serializableCheck: false }).concat(logger);
 		} else {
 			return getDefaultMiddleware({ serializableCheck: false });
 		}
 	},
-	devTools: process.env.NODE_ENV === 'development',
+	devTools: env,
 })
 export default store
